e102987895a03e37e49df928f64c1074270740fa
FMT <FMT157@126.com>
2016-03-09 23:11:22 +0800

    创建项目

---
+++ app/src/main/java/album/fmt/com/allbum/MainActivity.java
@@ -0,0 +1,14 @@
+package album.fmt.com.allbum;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
